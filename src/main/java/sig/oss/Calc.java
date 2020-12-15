429ad74b8766b4a6498589acf79d0c0b931fd621
comstudy21 <comstudy21@naver.com>
2020-12-15 22:37:06 +0900

    Add comments to add method in Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -3,6 +3,7 @@ package sig.oss;
 public class Calc {
     public int add(int x, int y) {
         System.out.print(">>>>>>> Run add method ....");
+        System.out.print(">>>>>>> Before add git ...");
         return x+y;
     }
 }
