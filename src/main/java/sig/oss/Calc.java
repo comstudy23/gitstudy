4f1b87c0eb9e48c77f6790892160fd6b4d3dee72
comstudy21 <comstudy21@naver.com>
2020-12-16 21:19:54 +0900

    Add a substraction method~

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -3,7 +3,11 @@ package sig.oss;
 public class Calc {
     public int add(int x, int y) {
         System.out.print(">>>>>>> Run add method ....");
-        System.out.print(">>>>>>> Before add git ...");
         return x+y;
+    }
+
+    public int sub(int x, int y) {
+        System.out.print(">>>>>>> Run sub method ...");
+        return x -y;
     }
 }
--- src/test/java/sig/oss/CalcTest.java
+++ src/test/java/sig/oss/CalcTest.java
@@ -10,4 +10,11 @@ public class CalcTest {
         Calc c = new Calc();
         assertEquals(30, c.add(10, 20));
     }
+
+    @Test
+    public void testSub() {
+        System.out.println(">>>>>>>>>> Run testSub() method .....");
+        Calc c = new Calc();
+        assertEquals(-10, c.sub(10, 20));
+    }
 }
